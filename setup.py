add8779d5eb4c903377752899e65a90d2ac5706b
Gokhan <gokhanarman89@gmail.com>
2022-03-15 14:46:30 +0300

    ("Counting_GUI_v1.py", icon = lion.ico)daki base=base kaldırıldı

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import cx_Freeze
 import sys
 
-import numpy
-import cv2
 
-base = None
+# base = None
 
-if sys.platform == 'win32':
-    base = "Win32GUI"
+# if sys.platform == 'win64':
+#     base = "Win64GUI"
 
-executables = [cx_Freeze.Executable("Counting_GUI_v1.py", base = base, icon = 'lion.ico')]
+executables = [cx_Freeze.Executable("Counting_GUI_v1.py", icon = 'lion.ico')]
 
 cx_Freeze.setup(
     name = "Counting_GUI_v1.py",
